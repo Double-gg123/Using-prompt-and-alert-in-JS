01f190db88be33f77c350c80c4fa7983d8d3e21a
Goodness GIBZ <71548390+Double-gg123@users.noreply.github.com>
2023-02-12 20:25:25 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let firstname = prompt("What is your firstname?","Goodness" );
+let lastname = prompt("What is your lastname?", "Gibendi");
+
+// Show an alert with a message
+alert("Hello, " + firstname+""  +lastname);
+
+let age = parseInt(prompt("What is your age?","19"));
+
+alert("Hello, " + firstname + "! You are " + age + " years old.");
